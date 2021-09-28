2741bd6b54eac001aeb270c88ccd9ea6c13392ba
Allosius27 <yanis.queroix@orange.fr>
2021-09-28 16:34:38 +0200

    Add UI Tooltip HealthBar

---
+++ SciFiProject/Assets/005_Scripts/UI/UITooltipSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SciFiProject.Core.UI.Tooltips;
+
+namespace SciFiProject.UI
+{
+    public class UITooltipSpawner : TooltipSpawner
+    {
+        public override bool CanCreateTooltip()
+        {
+            return true;
+        }
+
+        public override void UpdateTooltip(GameObject tooltip)
+        {
+            
+        }
+    }
+}
+
